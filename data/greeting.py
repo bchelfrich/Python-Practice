341d7a6a425a3a2e0c0909096494149614d7b03d
Brandon Helfrich <100109061+bchelfrich@users.noreply.github.com>
2024-01-20 15:18:59 -0600

    Uploaded files that were already complete
    greeting.py - basic greeting using strings
    introduction.py - expanded upon greeting.py but added an integer
    pig_game.py - making the dice game "Pig"

---
+++ data/greeting.py
@@ -0,0 +1,8 @@
+def greeting(fname, lname, location):
+    print("Hello %s %s! I am also from %s." % (fname, lname, location))
+
+first = "Peter"
+last = "Parker"
+state = "New York"
+
+greeting(first, last, state)
---
+++ data/introduction.py
@@ -0,0 +1,11 @@
+def introduction(rfname, rlname, fname, lname, years, major):
+    print("Hello %s %s, my name is %s %s. I graduated %.0f years ago with my degree in %s." % (rfname, rlname, fname, lname, years, major))
+
+rfirst = "Tony"
+rlast = "Stark"
+first = "Peter"
+last = "Parker"
+years = 2
+major = "Cybersecurity"
+
+introduction(rfirst, rlast, first, last, years, major)
